856cab21e4665e9863e8027ed62c94843144b440
MandarKM <91545003+MandarKM@users.noreply.github.com>
2022-10-21 11:06:11 +0530

    Update Jscript.js

--- portofolio/Jscript.js
+++ portofolio/Jscript.js
@@ -1,13 +1,1 @@
-function splitScroll(){
-    const contoller = new ScrollMagic.Contoller;
-    new scrollMagic.Scene({
-        duration: 500,
-        triggerElment: 'about-title',
-    })
-    .addIndicators()
-    .addTo(contoller);
-}
 
-
-
-splitScroll();
